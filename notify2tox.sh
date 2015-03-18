6dc061a44e3041f1a7ee24e66bf06bb2875c3a35
Daniel Dörrhöfer <dando@gmx.de>
2015-03-18 11:27:26 +0100

    first commit.

---
+++ notify2tox.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+regf="(^:|^urgency|^normal|^low|^icon_data)";
+interface=org.freedesktop.Notifications
+member=Notify
+
+dbus-monitor --monitor "interface='$interface',member='$member'" |
+while read -r line; do
+ 	temp=$(echo $line | 
+		tr -d '"' | 
+		egrep -o "^string.+" | 
+		sed 's/\(^string\s\)\(.*\)/\2/')
+
+	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp" 
+	then
+		echo "$temp" > "$1/text_in";
+	fi
+done
