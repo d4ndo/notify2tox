68d678651b36f4135fa855bb470b01d36e6aa228
Daniel Dörrhöfer <dando@gmx.de>
2015-03-19 11:54:54 +0100

    Added missing quotations

--- notify2tox.sh
+++ notify2tox.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 regf="(^:|^urgency|^normal|^low|^icon_data)";
-interface=org.freedesktop.Notifications
-member=Notify
+interface="org.freedesktop.Notifications";
+member="Notify";
 
 dbus-monitor --monitor "interface='$interface',member='$member'" |
 while read -r line; do
- 	temp=$(echo $line |
+ 	temp="$(echo "$line" |
 		tr -d '"' |
 		egrep -o "^string.+" |
-		sed 's/\(^string\s\)\(.*\)/\2/')
+		sed 's/\(^string\s\)\(.*\)/\2/')";
 
 	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp"
 	then
