2594141217889dcbea878ba33d6d16c64bd6cf93
Daniel Dörrhöfer <dando@gmx.de>
2015-03-19 10:59:35 +0100

    only send notify message, when receiver is online. removed trailing whitespace.

--- notify2tox.sh
+++ notify2tox.sh
@@ -6,13 +6,16 @@ member=Notify
 
 dbus-monitor --monitor "interface='$interface',member='$member'" |
 while read -r line; do
- 	temp=$(echo $line | 
-		tr -d '"' | 
-		egrep -o "^string.+" | 
+ 	temp=$(echo $line |
+		tr -d '"' |
+		egrep -o "^string.+" |
 		sed 's/\(^string\s\)\(.*\)/\2/')
 
-	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp" 
+	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp"
 	then
-		echo "$temp" > "$1/text_in";
+		if egrep -q "1" <<< "$1/online"
+		then
+			echo "$temp" > "$1/text_in";
+		fi
 	fi
 done
