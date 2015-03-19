066d0c8bf926d6760ecbd68a166cf1ed25d0bf31
Daniel Dörrhöfer <dando@gmx.de>
2015-03-19 15:30:42 +0100

    Indent

--- notify2tox.sh
+++ notify2tox.sh
@@ -6,15 +6,11 @@ member="Notify";
 
 dbus-monitor --monitor "interface='$interface',member='$member'" |
 while read -r line; do
- 	temp="$(echo "$line" |
-		tr -d '"' |
-		egrep -o "^string.+" |
+ 	temp="$(echo "$line" | tr -d '"' | egrep -o "^string.+" |
 		sed 's/\(^string\s\)\(.*\)/\2/')";
 
-	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp"
-	then
-		if egrep -q "1" <<< "$1/online"
-		then
+	if [ -n "$temp" ] && ! egrep -q "$regf" <<< "$temp"; then
+		if egrep -q "1" <<< "$1/online"; then
 			echo "$temp" > "$1/text_in";
 		fi
 	fi
